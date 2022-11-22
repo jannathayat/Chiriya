d8cc888939f234773aa1e414009ec9d95f94fb41
Jannat Hayat <jannathayat4@gmail.com>
2022-11-21 21:52:20 -0500

    added a comment

--- Chiriya/Chiriya/AppData.swift
+++ Chiriya/Chiriya/AppData.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 //This is Data for my application
+
+//this data is used for table view
 struct AppData {
     let userName: String
     let tweet: String
     let image: UIImage
 }
+
